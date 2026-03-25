39b7acc446d5cd9afd84eff4d8ed440c534b3985
Palak Sharma <palaksharma03210@gmail.com>
2026-03-25 06:47:12 +0530

    Day 1

---
+++ Message_print.cpp
@@ -0,0 +1,17 @@
+// Program to print a message
+#include<iostream>
+// iostream is a cpp library used to take input and give output.
+int main() {
+    // std is used to call cout here.
+    std:: cout<<"Hey!"<<"\n";
+    std:: cout<<"What's up ?"<<"\n";
+    std:: cout<<"Let's start learning Cpp"<<"\n";
+    return 0 ;
+}
+
+// cpp generalized skeleton.
+// # include <  write library to be used here >
+// using name space std ; ( we can use it instead of writing std again and again.)
+// int main () { 
+// main code
+// return 0 ;  }

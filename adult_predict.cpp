69780720ef1a626a4604c96f2733ad7a92d84d31
Palak Sharma <palaksharma03210@gmail.com>
2026-03-26 22:40:24 +0530

    Day 2
    This program prompts the user for their age and determines if they are an adult based on the input.

---
+++ adult_predict.cpp
@@ -0,0 +1,18 @@
+// Program that takes an input of age and prints if you are an adult or not.
+
+#include<iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << " Enter your age : ";
+    cin >> age;
+
+    if ( age >= 18 ){
+        cout << "You are an adult!";
+    }
+
+    else {
+        cout << "You are not an adult!";
+    }
+    return 0;
+}

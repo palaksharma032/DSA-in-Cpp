6133bf61457d24bdc5ce661fb185b0ef451ab44c
Palak Sharma <palaksharma03210@gmail.com>
2026-03-29 22:18:02 +0530

    Delete Printing_2_integers.cpp

--- Printing_2_integers.cpp
+++
@@ -1,9 +0,0 @@
-// Program to print two integers
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b ; 
-    cin>>a>>b;
-    cout<<"1st value : "<< a << " 2nd value : "<< b;
-    return 0 ;
-}

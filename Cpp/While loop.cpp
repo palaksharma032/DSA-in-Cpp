99950d1ca604bdaa658a8804a325f3ce90b8c886
Palak Sharma <palaksharma03210@gmail.com>
2026-03-31 05:20:48 +0530

    Creating a while loop

---
+++ Cpp/While loop.cpp
@@ -0,0 +1,11 @@
+// Program to create a while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int i =1;
+    while (i<=10) {
+        cout<<"While loop is executed"<< i << endl ;
+        i=i+1;
+    }
+    return 0 ;
+}

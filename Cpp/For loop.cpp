ad3caae0a6e3d048711116667a597a994f436bd5
Palak Sharma <palaksharma03210@gmail.com>
2026-03-30 21:12:45 +0530

    Using a for loop

---
+++ Cpp/For loop.cpp
@@ -0,0 +1,9 @@
+// Program using for loop
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1 ; i<=10 ; i++){
+        cout<<" For loop executing "<< i << endl;
+    }
+    return 0;
+}

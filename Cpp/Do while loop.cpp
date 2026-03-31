e96257b103fc9b832dcbbd339b5d26c6f4dfc789
Palak Sharma <palaksharma03210@gmail.com>
2026-03-31 05:30:48 +0530

    Creating do while loop
    Implement a do while loop that prints a message and increments a counter.

---
+++ Cpp/Do while loop.cpp
@@ -0,0 +1,13 @@
+// Program to create a do while loop.
+#include<iostream>
+using namespace std;
+int main(){
+    int i=3;
+    do{
+        cout<< " Do while loop is executing " << i<< endl;
+        i=i+1;
+    } while (i<=1) ;
+    {
+    cout << i << endl;}
+    return 0 ;
+}

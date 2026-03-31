c29b8e660885bc83ebedc89f875e5681f4497a9c
Palak Sharma <palaksharma03210@gmail.com>
2026-03-31 05:42:40 +0530

    Use function to print a message

---
+++ Cpp/Message using functions.cpp
@@ -0,0 +1,10 @@
+// Print a message using a function.
+#include<iostream>
+using namespace std;
+void printmessage (){
+    cout<<" I am learning functions ";
+}
+int main(){
+    printmessage();
+    return 0 ;
+}
